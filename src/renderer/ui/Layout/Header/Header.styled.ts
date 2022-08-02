c4514219761b560b822550432ebf43864d78e4cd
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-08-02 16:12:51 -0300

    Add Header component

--- src/renderer/ui/Layout/Header/Header.styled.ts
+++ src/renderer/ui/Layout/Header/Header.styled.ts
@@ -1,6 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import theme from 'renderer/config/theme';
+import { ButtonIcon } from 'renderer/ui/Button/Button.styled';
+
+type GridButtonProps = {
+  active?: boolean;
+};
 
 export const Container = styled.header`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  padding: ${theme.spacing[4]};
+  background: ${theme.colors.neutral[900]};
+`;
+
+export const GridButton = styled(ButtonIcon)<GridButtonProps>`
+  ${({ active }) =>
+    active &&
+    css`
+      color: ${theme.colors.white};
+    `}
 `;
--- src/renderer/ui/Layout/Header/Header.tsx
+++ src/renderer/ui/Layout/Header/Header.tsx
@@ -1,5 +1,36 @@
+import { Circle, CirclesFour, CirclesThree, UsersThree } from 'phosphor-react';
+import { Badge, Box, Typography, Logo } from 'renderer/ui';
+
 import * as S from './Header.styled';
 
 export default function Header() {
-  return <S.Container>Design Sprint Meeting</S.Container>;
+  return (
+    <S.Container>
+      <Box direction="horizontal" spacing={4} align="center">
+        <Logo />
+        <Box direction="horizontal" spacing={3} align="center">
+          <Typography.Title as="h1" size="paragraph">
+            Design Sprint Meeting
+          </Typography.Title>
+          <Badge variant="neutral">
+            <Badge.Icon>
+              <UsersThree size={20} />
+            </Badge.Icon>
+            Teams
+          </Badge>
+        </Box>
+      </Box>
+      <Box direction="horizontal" spacing={2}>
+        <S.GridButton variant="neutral" active>
+          <Circle size={20} />
+        </S.GridButton>
+        <S.GridButton variant="neutral">
+          <CirclesThree size={20} />
+        </S.GridButton>
+        <S.GridButton variant="neutral">
+          <CirclesFour size={20} />
+        </S.GridButton>
+      </Box>
+    </S.Container>
+  );
 }
